443e6feee60e853e398449756925006f8ed6f971
Gera! <67428423+GeraZerbetto@users.noreply.github.com>
2024-06-22 15:51:54 -0300

    .

--- estudiantes/src/main/java/aed/SistemaSIU.java
+++ estudiantes/src/main/java/aed/SistemaSIU.java
@@ -1,12 +1,21 @@
 package aed;
 
 import java.lang.reflect.Array;
-import java.util.*;
-import java.lang.String;
+import java.util.ArrayList;
 
-public class SistemaSIU {
-    private DictTrie<String, Carrera> carreras;
-    private DictTrie<String, Integer> alumnos;
+public class SistemaSIU { // Invariante: no se puede repetir un mismo LU en una materia.
+    // Cada alumno inscripto en una materia debe estar en trie alumnos
+    // Cada alumno puede estar inscripto únicamente a materias de su/s carrera/s
+    // Los nombres de las materias dentro de una carrera deben ser únicos
+    // Todos los strings LU tienen una misma longitud maxima
+    // cada alumno tiene un LU unico
+    // Cada alumno aparece en tantas materias como indique su cantidad de materias
+    // inscriptas, y estas deben
+    // ser diferentes entre si
+    // la cantidad de inscriptos en una materia debe ser menor o igual a la cantidad
+    // de claves del trie alumnos
+    private DictTrie<Carrera> carreras;
+    private DictTrie<Integer> alumnos;
 
     private class Tupla<A, B> {
         private A primero;
@@ -26,75 +35,157 @@ public class SistemaSIU {
         }
     }
 
-    private class NodoTrie<T> {
-        NodoTrie[] nodoHijo;
-        Boolean esHoja;
-        T valor;
+    private class DictTrie<V> { // Invariante: no se llega por 2 claves al mismo nodo
+        // si los nodos no tienen significado (val), tienen hijos.
+        // No hay nodos huerfanos (no alcanzables por la raiz)
+        // Los valores no son nulos si tienen una clave asociada
+        // Las claves no son vacías
+        private Nodo raiz;
 
-        public NodoTrie(int tamaño) {
-            this.nodoHijo = new NodoTrie[tamaño];
-            this.esHoja = false;
-            this.valor = null;
+        private class Nodo {
+            ArrayList<Nodo> sig; // O(1)
+            String clave;
+            V val; // O(1)
+            int hijosNoNulo; // O(1)
+
+            public Nodo() {
+                sig = new ArrayList<Nodo>();
+                // al ejecutarse siempre 255 veces tiene complejidad O(255) = O(1)
+                for (int i = 0; i < 256; i++) {
+                    sig.add(null);
+                }
+                hijosNoNulo = 0; // O(1)
+                val = null;
+            }
+
         }
-    }
 
-    private class DictTrie<String, V> {
-        // TODO: implementar un diccionario con un trie, con clave tipo K y valor tipo
-        // V.
-        NodoTrie raiz;
-
-        public DictTrie(int tamaño) {
-            this.raiz = new NodoTrie<V>(tamaño);
+        public DictTrie() {
+            raiz = new Nodo(); // O(1)
         }
 
         public Boolean pertenece(String clave) {
-            // TODO: implementar
-            throw new UnsupportedOperationException("Método no implementado aún");
+            Nodo actual = raiz; // O(1)
+            // este for tiene complejidad O(|s|) ya que en el peor caso cicla por todos los
+            // caracteres del String
+            for (int i = 0; i < clave.length(); i++) {
+                int c = (int) clave.charAt(i); // O(1)
+                if (actual.sig.get(c) == null) { // O(1)
+                    return false; // O(1)
+                } else {
+                    actual = actual.sig.get(c); // O(1)
+                }
+            }
+            return actual.val != null; // O(1)
         }
 
-        public void agregar(java.lang.String clave, V valor) {
-            NodoTrie nodoActual = this.raiz;
-            java.lang.String hola = "hola";
-            int longitudClave = clave.length();
-            // TODO: implementar
+        public void agregar(String clave, V valor) {
+            Nodo actual = raiz; // O(1)
+            // este for tiene complegidad O(|s|) ya que siempre cicla por todos los
+            // caracteres del String
+            for (int i = 0; i < clave.length(); i++) {
+                int c = (int) clave.charAt(i); // supongo que es O(1)
+                if (actual.sig.get(c) == null) { // O(1)
+                    Nodo n = new Nodo();
+                    actual.sig.set(c, n); // O(1)
+                    actual.hijosNoNulo++; // O(1)
+                }
+                actual = actual.sig.get(c); // O(1)
+            }
+            actual.val = valor; // O(1)
+            actual.clave = clave;
+        }
+
+        public void quitar(String clave) {
+            Nodo actual = raiz; // O(1)
+            Nodo ultimoUtil = raiz; // O(1)
+            int ultimoCUtil = clave.charAt(0); // O(1)
+            // este for tiene complegidad O(|s|) ya que siempre cicla por todos los
+            // caracteres del String
+            for (int i = 0; i < clave.length(); i++) {
+                int c = (int) clave.charAt(i); // O(1)
+                if (actual.hijosNoNulo > 1) { // O(1)
+                    ultimoCUtil = c; // O(1)
+                    ultimoUtil = actual; // O(1)
+                }
+                actual = actual.sig.get(c); // O(1)
+            }
+            if (actual.hijosNoNulo == 0) { // O(1)
+                ultimoUtil.sig.set(ultimoCUtil, null); // O(1)
+            } else {
+                actual.val = null; // O(1)
+                actual.clave = null;
+            }
         }
 
         public V devolver(String clave) {
-            // TODO: implementar
-            throw new UnsupportedOperationException("Método no implementado aún");
+            Nodo actual = raiz; // O(1)
+            for (int i = 0; i < clave.length(); i++) { // O(|s|) -> cicla por caracteres de string
+                int c = (int) clave.charAt(i);
+                if (actual.sig.get(c) == null) {
+                    return null;
+                } else {
+                    actual = actual.sig.get(c);
+                }
+            }
+            return actual.val;
         }
 
-        public void remover(String clave) {
-            // TODO: implementar
-            throw new UnsupportedOperationException("Método no implementado aún");
+        public String[] listar() { // Se recorren todas las claves (O(n*max(k)))-> n = cantidad de claves,
+            // max(k) = maximo tamaño de clave
+            ArrayList<String> list = new ArrayList<String>();
+
+            list = listaRecursiva(list, raiz);
+
+            String[] res = new String[list.size()];
+            res = list.toArray(res);
+            return res;
+        }
+
+        private ArrayList<String> listaRecursiva(ArrayList<String> lista, Nodo actual) {
+            if (actual.clave != null) {
+                lista.add(actual.clave);
+            }
+            if (actual.hijosNoNulo > 0) {
+                for (int i = 0; i < 256; i++) {
+                    if (actual.sig.get(i) != null) {
+                        lista = listaRecursiva(lista, actual.sig.get(i));
+                    }
+                }
+            }
+            return lista;
         }
     }
 
-    // CREO QUE ESTA CLASE ESTÁ LISTA
-    private class Materia {
+    private class Materia { // Invariante: el largo de listaAlumnos es igual a inscriptosMateria.
+        // listaSinonimos no tiene tuplas duplicadas
+        // El largo de plantelDocente es 4
+        // listaAlumnos no tiene alumnos repetidos
+        // Los int en plantelDocente y inscriptosMateria son mayores o iguales a 0
+
         private int[] plantelDocente;
         private ListaEnlazada<Tupla<Carrera, String>> listaSinonimos;
         private ListaEnlazada<String> listaAlumnos;
         private int inscriptosMateria;
 
         public Materia() {
-            this.plantelDocente = new int[] { 0, 0, 0, 0 };
-            this.listaSinonimos = new ListaEnlazada<Tupla<Carrera, String>>();
-            this.listaAlumnos = new ListaEnlazada<String>();
-            this.inscriptosMateria = 0;
+            this.plantelDocente = new int[] { 0, 0, 0, 0 }; // O(1)
+            this.listaSinonimos = new ListaEnlazada<Tupla<Carrera, String>>(); // O(1)
+            this.listaAlumnos = new ListaEnlazada<String>(); // O(1)
+            this.inscriptosMateria = 0; // O(1)
         }
 
         public void agregarSinonimo(Carrera carrera, String nombreMateria) {
-            Tupla<Carrera, String> nuevoSinonimo = new Tupla<Carrera, String>(carrera, nombreMateria);
-            this.listaSinonimos.agregarAtras(nuevoSinonimo);
+            Tupla<Carrera, String> nuevoSinonimo = new Tupla<Carrera, String>(carrera, nombreMateria); // O(1)
+            this.listaSinonimos.agregarAtras(nuevoSinonimo); // O(1)
         }
 
         public void inscribirAlumno(String alumno) {
-            this.inscriptosMateria += 1;
-            this.listaAlumnos.agregarAtras(alumno);
+            this.inscriptosMateria += 1; // O(1)
+            this.listaAlumnos.agregarAtras(alumno); // O(1)
         }
 
-        public void agregarDocente(CargoDocente cargo) {
+        public void agregarDocente(CargoDocente cargo) { // O(1)
             switch (cargo) {
                 case PROF:
                     this.plantelDocente[0] += 1;
@@ -110,15 +201,15 @@ public class SistemaSIU {
             }
         }
 
-        public int[] plantelDocente() {
+        public int[] plantelDocente() { // O(1)
             return plantelDocente;
         }
 
-        public int inscriptos() {
+        public int inscriptos() { // O(1)
             return inscriptosMateria;
         }
 
-        public boolean excedido() {
+        public boolean excedido() { // O(1)
             Boolean res;
             Boolean condProf = 250 * plantelDocente[0] < inscriptosMateria;
             Boolean condJTP = 100 * plantelDocente[1] < inscriptosMateria;
@@ -129,39 +220,49 @@ public class SistemaSIU {
         }
 
         public void cerrarMateria() {
-            Iterador<String> iterAlumnos = this.listaAlumnos.iterador();
-            while (iterAlumnos.haySiguiente()) {
-                String alumno = iterAlumnos.siguiente();
-                int nMateriasAlumno = alumnos.devolver(alumno);
-                nMateriasAlumno -= 1;
-                alumnos.agregar(alumno, nMateriasAlumno);
-            }
+            Iterador<String> iterAlumnos = this.listaAlumnos.iterador(); // O(1)
+            while (iterAlumnos.haySiguiente()) { // se ejecuta k veces
+                String alumno = iterAlumnos.siguiente(); // O(1)
+                int nMateriasAlumno = alumnos.devolver(alumno); // O(1)
+                nMateriasAlumno -= 1; // O(1)
+                alumnos.agregar(alumno, nMateriasAlumno); // O(1)
+            } // O(1) + sum(k)(O(1)) = O(1) + k*O(1) = O(k). k = longitud de listaAlumnos =
+              // cantidad de alumnos
+              // = Em. Entonces O(k) = O(Em)
 
-            Iterador<Tupla<Carrera, String>> iterSinonimos = this.listaSinonimos.iterador();
-            while (iterSinonimos.haySiguiente()) {
-                Tupla<Carrera, String> par = iterSinonimos.siguiente();
-                Carrera carreraDestino = par.devolverPrimero();
-                String nombreMateria = par.devolverSegundo();
-                carreraDestino.remover(nombreMateria);
-            }
-        }
-    }
+            Iterador<Tupla<Carrera, String>> iterSinonimos = this.listaSinonimos.iterador(); // O(1)
+            while (iterSinonimos.haySiguiente()) { // se ejectuta h veces
+                Tupla<Carrera, String> par = iterSinonimos.siguiente(); // O(1)
+                Carrera carreraDestino = par.devolverPrimero(); // O(1)
+                String nombreMateria = par.devolverSegundo(); // O(1)
+                carreraDestino.remover(nombreMateria); // O(|m|)
+            } // este while recorre listaSinonimos cuya longitud h indica la cantidad de
+              // nombres de la materia
+        } // y en cada iteracion se elimina c/u de esos nombres de sus respectivas
+          // carreras (O(|m|)). Así que se puede
+          // simplificar la complejidad como la sumatoria de la longitud de c/u de los
+          // nombres de la materia
+          // sum(n E Nm)(|n|)
+    } // cerrarMateria = O(Em + sum(n E Nm)(|n|))
 
     private class Carrera {
-        private DictTrie<String, Materia> materias;
+        private DictTrie<Materia> materias;
+
+        public Carrera() {
+            this.materias = new DictTrie<Materia>(); // O(1)
+        }
 
         public void agregar(String nombreMateria, Materia materia) {
-            this.materias.agregar(nombreMateria, materia);
+            materias.agregar(nombreMateria, materia); // O(|m|)
         }
 
         public Materia devolver(String nombreMateria) {
-            return this.materias.devolver(nombreMateria); // Ver implementación en el trie
+            return materias.devolver(nombreMateria); // O(|m|)
         }
 
         public void remover(String nombreMateria) {
-            this.materias.remover(nombreMateria); // Ver implementación en el trie
+            this.materias.quitar(nombreMateria); // O(|m|)
         }
-        // TODO: definir e implementar los métodos de la clase.
 
     }
 
@@ -172,86 +273,80 @@ public class SistemaSIU {
         PROF
     }
 
-    // LISTO
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias) {
-
-        this.carreras = new DictTrie<String, Carrera>();
-        this.alumnos = new DictTrie<String, Integer>();
-
-        for (InfoMateria materia : infoMaterias) {
-            Materia nuevaMateria = new Materia();
-            for (ParCarreraMateria par : materia.getParesCarreraMateria()) {
-                String nombreCarrera = par.getCarrera();
-                Carrera carreraDestino;
-                if (!(this.carreras.pertenece(nombreCarrera))) {
-                    carreraDestino = new Carrera();
-                    carreras.agregar(nombreCarrera, carreraDestino);
+        this.carreras = new DictTrie<Carrera>(); // O(1)
+        this.alumnos = new DictTrie<Integer>(); // O(1)
+        for (InfoMateria materia : infoMaterias) { // se ejecuta k veces (k=longitud infoMaterias = cantMaterias = |M|)
+            Materia nuevaMateria = new Materia(); // O(1)
+            for (ParCarreraMateria par : materia.getParesCarreraMateria()) { // ejecuta h veces (h=cant nombres de
+                                                                             // materia =|Nm|por materia)
+                String nombreCarrera = par.getCarrera(); // O(1)
+                Carrera carreraDestino; // O(1)
+                if (!(this.carreras.pertenece(nombreCarrera))) { // O(|c|)
+                    carreraDestino = new Carrera(); // O(1)
+                    carreras.agregar(nombreCarrera, carreraDestino); // O(|c|)
                 } else {
-                    carreraDestino = carreras.devolver(nombreCarrera);
+                    carreraDestino = carreras.devolver(nombreCarrera); // O(|c|)
                 }
-                carreraDestino.agregar(par.getNombreMateria(), nuevaMateria);
-                nuevaMateria.agregarSinonimo(carreraDestino, par.getNombreMateria());
+                carreraDestino.agregar(par.getNombreMateria(), nuevaMateria); // O(|m|) ->para cada materia
+                nuevaMateria.agregarSinonimo(carreraDestino, par.getNombreMateria()); // O(1)
             }
         }
-        for (String alumno : libretasUniversitarias) {
-            this.alumnos.agregar(alumno, 0);
+        for (String alumno : libretasUniversitarias) { // ejecuta t veces (t=cantidad estudiantes=E)
+            this.alumnos.agregar(alumno, 0); // O(1) -> strings LU acotados
         }
-    }
+    } // O(E + sum(m E M)sum(n E Nm)(|n|) + sum(c E C)(|c|*|Mc|))
 
-    // LISTO
     public void inscribir(String estudiante, String carrera, String materia) {
-        Carrera carreraDestino = this.carreras.devolver(carrera);
-        Materia materiaDestino = carreraDestino.devolver(materia);
-        materiaDestino.inscribirAlumno(estudiante);
-        int nMateriasAlumno = this.alumnos.devolver(estudiante);
-        nMateriasAlumno += 1;
-        this.alumnos.agregar(estudiante, nMateriasAlumno);
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|) ->nombre de carrera
+        Materia materiaDestino = carreraDestino.devolver(materia); // O(|m|) -> nombre de materia
+        materiaDestino.inscribirAlumno(estudiante); // O(1)
+        int nMateriasAlumno = this.alumnos.devolver(estudiante); // O(1) -> porque LU son acotadas
+        nMateriasAlumno += 1; // O(1)
+        this.alumnos.agregar(estudiante, nMateriasAlumno); // O(1)
+    } // O(1) + O(|c|) + O(|m|) = O(|c| + |m|)
 
     // LISTO
     public void agregarDocente(CargoDocente cargo, String carrera, String materia) {
-        Carrera carreraDestino = this.carreras.devolver(carrera);
-        Materia materiaDestino = carreraDestino.devolver(materia);
-        materiaDestino.agregarDocente(cargo);
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|)
+        Materia materiaDestino = carreraDestino.devolver(materia); // O(|m|)
+        materiaDestino.agregarDocente(cargo); // O(1)
+    } // O(|c|+|m|)
 
-    // LISTO
     public int[] plantelDocente(String materia, String carrera) {
-        Carrera carreraDestino = this.carreras.devolver(carrera);
-        Materia materiaDestino = carreraDestino.devolver(materia);
-        return materiaDestino.plantelDocente();
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|)
+        Materia materiaDestino = carreraDestino.devolver(materia); // O(|m|)
+        return materiaDestino.plantelDocente(); // O(1)
+    } // O(|c|+|m|)
 
-    // LISTO
     public void cerrarMateria(String materia, String carrera) {
-        Carrera carreraDestino = this.carreras.devolver(carrera);
-        Materia materiaDestino = carreraDestino.devolver(materia);
-        materiaDestino.cerrarMateria();
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|)
+        Materia materiaDestino = carreraDestino.devolver(materia); // O(|m|)
+        materiaDestino.cerrarMateria(); // O(Em + sum(n E Nm)(|n|))
+    } // O(|c|+|m|+Em+sum(n E Nm)(|n|))
 
-    // LISTO
     public int inscriptos(String materia, String carrera) {
-        Carrera carreraDestino = this.carreras.devolver(carrera);
-        Materia materiaDestino = carreraDestino.devolver(materia);
-        return materiaDestino.inscriptos();
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|)
+        Materia materiaDestino = carreraDestino.devolver(materia); // O(|m|)
+        return materiaDestino.inscriptos(); // O(1)
+    } // O(|c|+|m|)
 
-    // LISTO
     public boolean excedeCupo(String materia, String carrera) {
-        Carrera carreraDestino = this.carreras.devolver(carrera);
-        Materia materiaDestino = carreraDestino.devolver(materia);
-        return materiaDestino.excedido();
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|)
+        Materia materiaDestino = carreraDestino.devolver(materia); // O(|m|)
+        return materiaDestino.excedido(); // O(1)
+    } // O(|c|+|m|)
 
     public String[] carreras() {
-        throw new UnsupportedOperationException("Método no implementado aún");
-    }
+        return carreras.listar();
+    }// O(sum(c E C)(|c|))
 
     public String[] materias(String carrera) {
-        throw new UnsupportedOperationException("Método no implementado aún");
-    }
+        Carrera carreraDestino = this.carreras.devolver(carrera); // O(|c|)
+        return carreraDestino.materias.listar();
+    }// O(|c| + sum(mc E Mc)(|mc|))
 
     public int materiasInscriptas(String estudiante) {
-        throw new UnsupportedOperationException("Método no implementado aún");
+        return alumnos.devolver(estudiante); // O(1) -> debido a LU ACOTADA
     }
 }
